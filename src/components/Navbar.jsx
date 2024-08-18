626a01986b2e6ed3bb9e737739167dba15bdb580
shayan <shayan.dota88@gmail.com>
2024-08-18 20:57:22 +0500

    frontendchanges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,47 @@
-import { useNavigate, useLocation } from "react-router-dom"
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import "../styles/Navbar.css"
+import { useNavigate, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import Logo from "../assets/logo.webp";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-function Navbar(){
-    const navigate = useNavigate();
-    const location = useLocation();
-    const btnText = "Login"
-    const handleClick = (e) => {
-        e.preventDefault();
-        if(location.pathname === '/register'){
-            navigate('/login')
-        }
-        else{
-            navigate('/register')
-        }
-        
+function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const btnText = "Login";
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (location.pathname === "/register") {
+      navigate("/login");
+    } else {
+      navigate("/register");
     }
+  };
 
-    return(
-        <div className="navbar">
-            <div className="nav-right">
-                <img src="" alt="logo" />
-            </div>
+  return (
+    <div className="navbar">
+      <div className="nav-right">
+        <img src={Logo} alt="logo" width={50} />
+        <h2>ConnectStud</h2>
+      </div>
 
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li>About</li>
-            </ul>
-            {location.pathname === '/login' ? (
-          <button onClick={handleClick}>Register</button>
-        ) : location.pathname === '/register' ? (
-            <button onClick={handleClick}>Login</button>
-        ) : (
-          <>
-            <button onClick={handleClick}>Register</button>
-          </>
-        )}
-
-        </div>
-    )
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>About</li>
+      </ul>
+      {location.pathname === "/login" ? (
+        <button onClick={handleClick}>Register<FontAwesomeIcon icon={faArrowRight} className="my-arrow" /></button>
+      ) : location.pathname === "/register" ? (
+        <button onClick={handleClick}>Login<FontAwesomeIcon icon={faArrowRight} className="my-arrow" /></button>
+      ) : (
+        <>
+          <button onClick={handleClick}>Register<FontAwesomeIcon icon={faArrowRight} className="my-arrow" /></button>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
